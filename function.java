0753d552d1932aa0405da39ccb0ead46d663c066
ilancheziyan0812 <115941292+ilancheziyan0812@users.noreply.github.com>
2023-02-23 17:13:01 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,14 @@
+class function
+{
+    public static  add(int a,int b)
+{
+   int c = a+b;
+   return(c);
+}
+public static void main(String [] args)
+{
+    add();
+    int result = c;
+    system.out.println(c);
+}
+}
